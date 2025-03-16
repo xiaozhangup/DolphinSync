9629b488893b6bf637e667a6203d3ae8d369bb21
Daniel Zhang <xiaozhangshiw@163.com>
2025-03-16 22:05:07 +0800

    style(config): 调整 Redis 配置格式
    - 修改 config.yml 中 Redis 配置的注释格式
    - 移除 DolphinSync.kt 中未使用的导入语句

--- src/main/kotlin/me/xiaozhangup/dolphin/DolphinSync.kt
+++ src/main/kotlin/me/xiaozhangup/dolphin/DolphinSync.kt
@@ -7,10 +7,7 @@ import me.xiaozhangup.dolphin.source.DolphinDataSource
 import me.xiaozhangup.dolphin.source.DolphinStatisticSource
 import me.xiaozhangup.dolphin.utils.CoroutineTask
 import org.bukkit.Bukkit
-import taboolib.common.LifeCycle
-import taboolib.common.platform.Awake
 import taboolib.common.platform.Plugin
-import taboolib.common.platform.event.SubscribeEvent
 import taboolib.common.platform.function.info
 import taboolib.module.configuration.Config
 import taboolib.module.configuration.Configuration
